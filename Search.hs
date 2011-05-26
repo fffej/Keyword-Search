966a4f07e423b5c58fc23ab390083c00652a1a3a
Jeff Foster <jfoster@acm.org>
2011-05-26 22:12:58 +0100

    get rid of some warnings

--- Search.hs
+++ Search.hs
@@ -4,15 +4,11 @@ import Indexer (getTerm)
 
 import qualified Data.Text as T
 import Database.Redis.Redis
-import Database.Redis.ByteStringClass
-import Data.Maybe (fromJust)
 import Data.List.Split
-import Control.Monad (liftM)
 
 data Query = Contains T.Text
            | And Query Query
            | Or Query Query
-           | Xor Query Query      
            deriving (Show,Eq,Read)
                     
 getKey :: Query -> T.Text                    
@@ -20,22 +16,20 @@ getKey = T.pack . show
                     
 query :: Redis -> Query -> IO T.Text
 query r q@(And lhs rhs) = do
-  lhs <- query r lhs
-  rhs <- query r rhs
+  lhs' <- query r lhs
+  rhs' <- query r rhs
   let key = getKey q
-  x <- zinterStore r key [lhs,rhs] [] SUM
+  _ <- zinterStore r key [lhs',rhs'] [] SUM
   return key
 
 query r q@(Or lhs rhs) = do
-  lhs <- query r lhs
-  rhs <- query r rhs
+  lhs' <- query r lhs
+  rhs' <- query r rhs
   let key = getKey q
-  x <- zunionStore r key [lhs,rhs] [] SUM
+  _ <- zunionStore r key [lhs',rhs'] [] SUM
   return key
 
-query r (Contains text) = do
-  let searchTerm = getTerm text
-  return searchTerm
+query _ (Contains text) = return (getTerm text)
   
 getQueryResponse :: Redis -> T.Text -> IO [T.Text]
 getQueryResponse r key = do
