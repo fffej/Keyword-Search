9719796fb8835670edde96cabb12e040a776fd2c
Jeff Foster <jfoster@acm.org>
2011-05-31 07:50:41 +0100

    some more simply tidying up

--- Search.hs
+++ Search.hs
@@ -10,11 +10,7 @@ import qualified Data.Text as T
 import Database.Redis.Redis
 import Data.List.Split (splitEvery)
 
-import Data.Either (either)
-
 import Text.ParserCombinators.Parsec
-import Text.ParserCombinators.Parsec.Combinator 
-import Text.ParserCombinators.Parsec.Char
 import Text.ParserCombinators.Parsec.Expr
 import Text.ParserCombinators.Parsec.Token  
 import Text.ParserCombinators.Parsec.Language 
@@ -36,7 +32,7 @@ queryLang = emptyDef { identStart = letter
 TokenParser { parens = m_parens
             , identifier = m_identifier
             , reserved = m_reserved
-            , whiteSpace = m_whiteSpace } = makeTokenParser queryLang
+            } = makeTokenParser queryLang
                                             
 queryParser :: Parser Query                 
 queryParser = buildExpressionParser table term <?> "query"
@@ -62,28 +58,22 @@ parseQuery q = either processError (Right . id) x
 processError :: ParseError -> Either String Query    
 processError p = Left (concatMap messageString $ errorMessages p)
     
-isLeft :: Either a b -> Bool
-isLeft (Left x) = True
-isLeft _ = False
+                 
+binaryOp :: Redis -> Query -> Query -> Query -> (Redis -> T.Text -> [T.Text] -> [a1] -> Aggregate -> IO a)-> IO T.Text
+binaryOp r q lhs rhs op = do
+  lhs' <- query r lhs
+  rhs' <- query r rhs
+  let key = getKey q
+  _ <- op r key [lhs',rhs'] [] SUM
+  _ <- expire r key 30
+  return key
 
 getKey :: Query -> T.Text      
 getKey = T.pack . show
 
 query :: Redis -> Query -> IO T.Text
-query r q@(And lhs rhs) = do
-  lhs' <- query r lhs
-  rhs' <- query r rhs
-  let key = getKey q
-  _ <- zinterStore r key [lhs',rhs'] [] SUM
-  return key
-
-query r q@(Or lhs rhs) = do
-  lhs' <- query r lhs
-  rhs' <- query r rhs
-  let key = getKey q
-  _ <- zunionStore r key [lhs',rhs'] [] SUM
-  return key
-
+query r q@(And lhs rhs) = binaryOp r q lhs rhs zinterStore
+query r q@(Or lhs rhs) = binaryOp r q lhs rhs zunionStore
 query _ (Contains text) = return (getTerm text)
   
 getQueryResponse :: Redis -> T.Text -> IO [T.Text]
@@ -91,7 +81,7 @@ getQueryResponse r key = do
   resp <- zrange r key (0,99999999) True 
   x <- fromRMultiBulk' resp
   let v = map head (splitEvery 2 x) :: [T.Text]
-  mapM (\x -> get r x >>= fromRBulk') v
+  mapM (\z -> get r z >>= fromRBulk') v
   
   
   
