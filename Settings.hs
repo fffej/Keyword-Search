2514ae6135a0a3af54bf41d1d0555764b42f3790
Jeff Foster <jfoster@acm.org>
2011-06-01 21:00:28 +0100

    if you want to host it, pointing things at localhost:3000 is probably a bad idea

--- Settings.hs
+++ Settings.hs
@@ -38,7 +38,7 @@ approot :: Text
 -- you would probably want it to be:
 -- > approot = "http://www.yesod.com"
 -- Please note that there is no trailing slash.
-approot = "http://localhost:3000"
+approot = "http://search.boringtosser.co.uk"
 #else
 approot = "http://localhost:3000"
 #endif
